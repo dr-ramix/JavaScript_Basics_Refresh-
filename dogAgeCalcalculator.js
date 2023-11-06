528b710722c53b468adff90d6b47b37605ca83fd
Seyed Ramtin Hosseini <74879491+dr-ramix@users.noreply.github.com>
2023-11-06 19:41:25 +0100

    Create dogAgeCalcalculator.js
    This is a simple javascript project that calculate the age of a person in dog years

---
+++ dogAgeCalcalculator.js
@@ -0,0 +1,20 @@
+//My age
+const myAge = 20;
+//Early years
+let earlyYears = 2;
+////Early years in dogs year
+earlyYears *= 10.5;
+//Later years
+let laterYears = myAge-2;
+////Later years in dogs year
+laterYears *=4;
+//Printing the value
+console.log(`Early Years: ${earlyYears}`)
+console.log(`Early Years: ${laterYears}`)
+
+const dogYears = earlyYears + laterYears;
+
+//Saving my name but witth Lowercase letters
+const myName = "Ramtin".toLowerCase();
+//Printing the result
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${dogYears} years old in dog years.`);
